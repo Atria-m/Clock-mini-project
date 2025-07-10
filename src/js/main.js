5acdd4de925986659b358570341337e245f7952e
Atria-m <atriamoiedy13@gmail.com>
2025-07-11 03:18:14 +0330

    rington

--- src/js/main.js
+++ src/js/main.js
@@ -77,7 +77,7 @@ window.addEventListener('DOMContentLoaded', () => {
   let alarmState = "noset";
   let isAlarmPlaying = false;
 
-  const ringtone = new Audio("/audio/ringtone.mp3");
+  const ringtone = new Audio("music/ring.mp3");
   ringtone.loop = true;
 
   for (let i = 23; i >= 0; i--) {
